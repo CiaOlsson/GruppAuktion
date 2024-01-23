5fd80929b56ac7f60131c54003fec244f150a5d6
CiaOlsson <146974056+CiaOlsson@users.noreply.github.com>
2024-01-23 15:14:26 +0100

    Update CreateAuction.jsx

---
+++ GruppAuktion/src/components/Views/CreateAuction.jsx
@@ -0,0 +1,14 @@
+import {useNavigate} from 'react-router'; 
+
+// Några inputs och en knapp för att skicka vidare formuläret. 
+const CreateAuction = () =>{
+    // const navigate = useNavigate();
+    return (<>
+        <h1>Lägg till auktionsobjekt</h1>
+        {/* <button onClick={() => navigate("/")}>Tillbaka med en knapp</button> */}
+    </>)
+    
+    }
+    
+export default CreateAuction;
+
