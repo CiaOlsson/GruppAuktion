ad91d3adf32a373ef24198c74b90e7721c26b835
Shedcape <shedcapeyt@gmail.com>
2024-01-25 09:39:35 +0100

    Lagt till react-router

---
+++ GruppAuktion/src/components/Views/AuctionsPage/components/AuctionList.jsx
@@ -0,0 +1,67 @@
+import React, {useEffect} from 'react'
+import {Container, Typography, Grid} from "@mui/material"
+import { useSearchContext } from '../../../../context/SearchContextProvider'
+
+const AuctionList = () => {
+    const {searchString} = useSearchContext();
+    let auctions = [];
+    useEffect(() => {
+        const getAuctions = async() => {
+            const auctions = await fetch("api/auktion/100")
+                        .then(res => res.json())
+                        .then(data => data)
+                        .catch(err => console.log(err)) 
+            console.log(auctions);
+        }
+        auctions = getAuctions()
+    }, [])
+    return (
+    <Container sx={{display:"flex", justifyContent:"center", alignItems:"center", flexDirection:"column", backgroundColor:"gray", width:"100%"}}>
+        <Typography variant="h4">Auktioner</Typography>
+        <Grid container spacing={3}>
+            <Grid item xs={3}>
+                <Typography variant="body1">Hello World!</Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <Typography variant="body1">Hello World!</Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <Typography variant="body1">Hello World!</Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <Typography variant="body1">Hello World!</Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <Typography variant="body1">Hello World!</Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <Typography variant="body1">Hello World!</Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <Typography variant="body1">Hello World!</Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <Typography variant="body1">Hello World!</Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <Typography variant="body1">Hello World!</Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <Typography variant="body1">Hello World!</Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <Typography variant="body1">Hello World!</Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <Typography variant="body1">Hello World!</Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <Typography variant="body1">Hello World!</Typography>
+            </Grid>
+
+        </Grid>
+    </Container>
+  )
+}
+
+export default AuctionList
